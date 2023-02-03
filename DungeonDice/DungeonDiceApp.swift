538b1b3231b32ae6819dd4920c7de4880306da72
Nadia <ivashchenko.nadia@gmail.com>
2023-02-03 20:19:09 +0200

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Students on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
