0802ecab0646a2ad3f5563e9bb3082283567170d
Nadia <ivashchenko.nadia@gmail.com>
2023-02-05 20:46:40 +0200

    4.5 Reflow buttons

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -26,37 +26,81 @@ struct ContentView: View {
         }
     }
     
-    @State private var dice: Dice = .four
     @State private var mainTitle = "Dungeon Dice"
     @State private var resultMessage = ""
     
+    @State private var buttonsLeftOver = 0
+    
+    let horizontalPadding: CGFloat = 16
+    let spacing: CGFloat = 10
+    let buttonWidth: CGFloat = 150
+            
     var body: some View {
-        VStack {
-            Text(mainTitle)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            
-            Spacer()
-            
-            Text(resultMessage)
-                .font(.title)
-            
-            Spacer()
-            
-            
-            VStack{
-                ForEach(Dice.allCases, id: \.self) { number in
-                    Button {
-                        resultMessage = number.message()
-                    } label: {
-                        Text("\(number.rawValue)-sided")
+        
+        GeometryReader { geo in
+            VStack {
+                Text(mainTitle)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                Spacer()
+                
+                Text(resultMessage)
+                    .font(.title)
+                
+                Spacer()
+                
+                LazyVGrid(columns: [
+                    GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)
+                ], alignment: .center) {
+                    ForEach(Dice.allCases.dropLast(buttonsLeftOver), id: \.self) { number in
+                        Button {
+                            resultMessage = number.message()
+                        } label: {
+                            Text("\(number.rawValue)-sided")
+                        }
+                        .frame(width: buttonWidth)
+                        .buttonStyle(.borderedProminent)
                     }
-                    .buttonStyle(.borderedProminent)
+                }
+                
+                HStack(alignment: .center, spacing: spacing) {
+                    ForEach(Dice.allCases.suffix(buttonsLeftOver), id: \.self) { number in
+                        Button {
+                            resultMessage = number.message()
+                        } label: {
+                            Text("\(number.rawValue)-sided")
+                        }
+                        .frame(width: buttonWidth)
+                        .buttonStyle(.borderedProminent)
+                    }
                 }
             }
+            .padding()
+            .onAppear {
+                arangeGridItems(geo: geo)
+                print("A")
+            }
+            .onChange(of: geo.size.width, perform: { _ in
+                arangeGridItems(geo: geo)
+                print("C")
+            })
         }
-        .padding()
     }
+    
+    func arangeGridItems(geo: GeometryProxy) {
+        var screenWidth = geo.size.width - (horizontalPadding * 2)
+        
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+        }
+        
+        let numberOfButtonPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+        buttonsLeftOver = Dice.allCases.count % numberOfButtonPerRow
+        print(numberOfButtonPerRow)
+        print(buttonsLeftOver)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
